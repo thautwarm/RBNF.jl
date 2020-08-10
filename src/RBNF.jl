9bc8d18fa55f9c2e77a007b83f6663d7a84f8c63
Taine Zhao <twshere@outlook.com>
2020-08-10 21:41:04 +0900

    update to prettyprint 0.2

--- src/RBNF.jl
+++ src/RBNF.jl
@@ -9,7 +9,7 @@ include("Lens.jl")
 include("NaiveParserc.jl")
 include("ParserGen.jl")
 
-PrettyPrint.pprint_impl(io, tk::Token{T}, indent, newline) where T = begin
+PrettyPrint.pp_impl(io, tk::Token{T}, indent) where T = begin
     print(io, "Token{$T}(str=$(tk.str), lineno=$(tk.lineno), colno=$(tk.lineno))")
 end
 end # module
