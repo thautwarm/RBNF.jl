5559ff2ef5c2eb1261369bdc1962448f7ebbed66
Taine Zhao <twshere@outlook.com>
2020-08-10 21:45:06 +0900

    fix pretty print

--- src/RBNF.jl
+++ src/RBNF.jl
@@ -9,7 +9,9 @@ include("Lens.jl")
 include("NaiveParserc.jl")
 include("ParserGen.jl")
 
-PrettyPrint.pp_impl(io, tk::Token{T}, indent) where T = begin
-    print(io, "Token{$T}(str=$(tk.str), lineno=$(tk.lineno), colno=$(tk.lineno))")
+PrettyPrint.pp_impl(io, tk::Token{T}, indent::Int) where T = begin
+    s = "Token{$T}(str=$(tk.str), lineno=$(tk.lineno), colno=$(tk.lineno))"
+    print(io, s)
+    return length(s) + indent
 end
 end # module
