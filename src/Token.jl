439a5e27ac99cdbb86ea5ab4135c8f1fbd62c1a4
Roger-luo <rogerluo.rl18@gmail.com>
2020-10-16 23:20:03 -0400

    add a convenient constructor for token

--- src/Token.jl
+++ src/Token.jl
@@ -9,3 +9,11 @@ struct Token{T}
     str    :: String
     span   :: Int32
 end
+
+"""
+    Token{T}(str::String; lineno::Int=0, colno::Int=0, offset::Int=0, span::Int=0)
+
+Create a Token of type `T` with content of given `str::String`.
+"""
+Token{T}(str::String; lineno::Int=0, colno::Int=0, offset::Int=0, span::Int=0) where T =
+    Token{T}(lineno, colno, offset, str, span)
