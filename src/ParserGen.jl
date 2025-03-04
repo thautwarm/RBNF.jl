37ea540eafa8eca9f2b90aa64371ff39c0bd18e2
Kristoffer <kcarlsson89@gmail.com>
2025-03-04 15:10:20 +0100

    fix matching on 1.12 to make package test pass again

--- src/ParserGen.jl
+++ src/ParserGen.jl
@@ -85,7 +85,7 @@ function collect_context(node)
             IsMacro{:token} =>
                 begin
                     collector = @λ begin
-                        :($name := $node) -> push!(tokens, (name, node))
+                        :($name := $node) => push!(tokens, (name, node))
                         a -> throw(a)
                     end
                 end
